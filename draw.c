c283c4d4c02d9b5ac9a18e1cab5c2ed282cf24b7
Michael Mackus <michaelmackus@gmail.com>
2018-07-13 19:27:40 -0700

    Rename map -> drawBuffer

--- draw.c
+++ draw.c
@@ -26,19 +26,19 @@ void deinit()
 
 // temporary global to hold previous map state, so we only draw what was changed
 // THANKS curses!
-char map[MAX_HEIGHT + MAX_MESSAGES][MAX_WIDTH];
+char drawBuffer[MAX_HEIGHT + MAX_MESSAGES][MAX_WIDTH];
 
 void render_mob(const Mob *mob);
 void render_messages(const char **messages);
 void render_level(Level *level);
-void draw(const char map[][MAX_WIDTH], const char prevMap[][MAX_WIDTH]);
+void draw(const char drawBuffer[][MAX_WIDTH], const char prevDrawBuffer[][MAX_WIDTH]);
 void update(const Dungeon *dungeon, const char **messages)
 {
     // fill previous map
-    char prevMap[MAX_HEIGHT + MAX_MESSAGES][MAX_WIDTH];
+    char prevDrawBuffer[MAX_HEIGHT + MAX_MESSAGES][MAX_WIDTH];
     for (int y = 0; y < MAX_HEIGHT + MAX_MESSAGES; ++y)
         for (int x = 0; x < MAX_WIDTH; ++x)
-            prevMap[y][x] = map[y][x];
+            prevDrawBuffer[y][x] = drawBuffer[y][x];
 
     // render onto our current map
     render_level(dungeon->level);
@@ -46,7 +46,7 @@ void update(const Dungeon *dungeon, const char **messages)
     render_messages(messages);
 
     // draw difference from old map & new map
-    draw(map, prevMap);
+    draw(drawBuffer, prevDrawBuffer);
 }
 
 /*************/
@@ -64,14 +64,14 @@ void render_level(Level *level)
         for (int x = 0; x < MAX_WIDTH; ++x)
         {
             // draw character, offset y by 5 for message area
-            map[y][x] = tile_symbol(level->tiles[y][x]);
+            drawBuffer[y][x] = tile_symbol(level->tiles[y][x]);
         }
     }
 }
 
 void render_mob(const Mob *mob)
 {
-    map[mob->y][mob->x] = mob->symbol;
+    drawBuffer[mob->y][mob->x] = mob->symbol;
 }
 
 void render_messages(const char **messages)
@@ -81,21 +81,21 @@ void render_messages(const char **messages)
         for (int x = 0; x < MAX_MESSAGE_LENGTH; ++x)
         {
             if (messages[y] != NULL)
-                map[y+MAX_HEIGHT][x] = messages[y][x];
+                drawBuffer[y+MAX_HEIGHT][x] = messages[y][x];
         }
     }
 }
 
 // only draw difference in map to curses window
-void draw(const char map[][MAX_WIDTH], const char prevMap[][MAX_WIDTH])
+void draw(const char drawBuffer[][MAX_WIDTH], const char prevDrawBuffer[][MAX_WIDTH])
 {
     for (int y = 0; y < MAX_HEIGHT + MAX_MESSAGES; ++y)
     {
         for (int x = 0; x < MAX_WIDTH; ++x)
         {
-            if (map[y][x] != prevMap[y][x])
+            if (drawBuffer[y][x] != prevDrawBuffer[y][x])
             {
-                mvaddch(y, x, map[y][x]);
+                mvaddch(y, x, drawBuffer[y][x]);
                 refresh();
             }
         }
