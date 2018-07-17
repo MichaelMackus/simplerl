ce53be5c126bd646fa5108e9f4360eaabb333545
Michael Mackus <michaelmackus@gmail.com>
2018-07-16 23:27:44 -0700

    Add different item attributes and weapons

--- item.c
+++ item.c
@@ -1,32 +1,17 @@
 #include "item.h"
 #include "random.h"
 
-Item *create_item(int depth, int type, const char **unknownItems, const char **knownItems)
+Item *generate_gold(int depth);
+Item *generate_weapon(int depth);
+Item *create_item(int depth, int type)
 {
-    Item *item = malloc(sizeof(Item));
-
-    if (item == NULL)
-        return NULL;
-
-    item->type = type;
-
     if (type == ITEM_GOLD)
-    {
-        // generate depth*50/2 - depth*50 gold
-        int amount = generate(depth*50/2, depth*50);
-        item->strength = amount;
-        item->unknownName = item->name = "Gold";
-    }
+        return generate_gold(depth);
+    else if (type == ITEM_WEAPON)
+        return generate_weapon(depth);
     // TODO rest of item types
-    else
-    {
-        // error case
-        free(item);
 
-        return NULL;
-    }
-
-    return item;
+    return NULL;
 }
 
 #define ITEM_SIZE_INCREMENT 10
@@ -63,4 +48,177 @@ int insert_item(Item *item, Items *items)
     ++items->count;
 
     return 1;
+}
+
+
+// type-specific item generators
+
+Item *generate_gold(int depth)
+{
+    Item *item = malloc(sizeof(Item));
+
+    if (item == NULL)
+        return NULL;
+
+    // generate depth*50/2 - depth*50 gold
+    item->amount = generate(depth*50/2, depth*50);
+    item->type = ITEM_GOLD;
+    item->name = item->unknownName = "Gold";
+
+    return item;
+}
+
+
+// weapons
+
+Item *club(Item *weapon)
+{
+    weapon->damage.max = 4;
+    weapon->damage.type = DAMAGE_BLUNT;
+    weapon->name = weapon->unknownName = "club";
+    return weapon;
+}
+
+Item *dagger(Item *weapon)
+{
+    weapon->damage.max = 4;
+    weapon->name = weapon->unknownName = "dagger";
+    return weapon;
+}
+
+Item *short_sword(Item *weapon)
+{
+    weapon->damage.max = 6;
+    weapon->name = weapon->unknownName = "short sword";
+    return weapon;
+}
+
+Item *mace(Item *weapon)
+{
+    weapon->damage.max = 6;
+    weapon->damage.type = DAMAGE_BLUNT;
+    weapon->name = weapon->unknownName = "club";
+    return weapon;
+}
+
+Item *quarterstaff(Item *weapon)
+{
+    weapon->damage.max = 8;
+    weapon->damage.type = DAMAGE_BLUNT;
+    weapon->damage.twoHanded = 1;
+    weapon->name = weapon->unknownName = "quarterstaff";
+    return weapon;
+}
+
+Item *long_sword(Item *weapon)
+{
+    weapon->damage.max = 8;
+    weapon->name = weapon->unknownName = "long sword";
+    return weapon;
+}
+
+Item *bastard_sword(Item *weapon)
+{
+    weapon->damage.max = 10;
+    weapon->damage.twoHanded = 1;
+    weapon->name = weapon->unknownName = "bastard sword";
+    return weapon;
+}
+
+Item *flail(Item *weapon)
+{
+    weapon->damage.max = 8;
+    weapon->damage.type = DAMAGE_BLUNT;
+    weapon->name = weapon->unknownName = "flail";
+    return weapon;
+}
+
+Item *masterwork_sword(Item *weapon)
+{
+    weapon->damage.min = 2;
+    weapon->damage.max = 10;
+    weapon->name = weapon->unknownName = "masterwork sword";
+    return weapon;
+}
+
+Item *masterwork_bastard_sword(Item *weapon)
+{
+    weapon->damage.min = 2;
+    weapon->damage.max = 12;
+    weapon->name = weapon->unknownName = "masterwork bastard sword";
+    return weapon;
+}
+
+Item *silver_sword(Item *weapon)
+{
+    weapon->damage.min = 2;
+    weapon->damage.max = 10;
+    weapon->damage.type = DAMAGE_SILVER;
+    weapon->name = weapon->unknownName = "silver sword";
+    return weapon;
+}
+
+
+Item *generate_weapon(int depth)
+{
+    Item *item = malloc(sizeof(Item));
+
+    if (item == NULL)
+        return NULL;
+
+    // weapon defaults
+    item->type = ITEM_WEAPON;
+    item->damage.type = DAMAGE_SLASH;
+    item->damage.min = 1;
+    item->damage.twoHanded = 0;
+
+    // percentile roll
+    int percent = generate(1, 100);
+
+    // TODO artifact/unique weapons
+
+    // generate different weapons based on depth
+    if (depth <= 2)
+    {
+        if (percent <= 25)
+            return club(item);
+        else if (percent <= 50)
+            return dagger(item);
+        else if (percent <= 75)
+            return short_sword(item);
+        else
+            return quarterstaff(item);
+    }
+    else if (depth <= 5)
+    {
+        if (percent <= 20)
+            return dagger(item);
+        else if (percent <= 35)
+            return quarterstaff(item);
+        else if (percent <= 50)
+            return mace(item);
+        else if (percent <= 75)
+            return long_sword(item);
+        else
+            return bastard_sword(item);
+    }
+    else
+    {
+        if (percent <= 20)
+            return long_sword(item);
+        else if (percent <= 35)
+            return bastard_sword(item);
+        else if (percent <= 75)
+            return flail(item);
+        else if (percent <= 85)
+            return masterwork_sword(item);
+        else if (percent <= 95)
+            return masterwork_bastard_sword(item);
+        else
+            return silver_sword(item);
+    }
+
+    free(item);
+
+    return NULL;
 }
--- item.h
+++ item.h
@@ -4,21 +4,42 @@
 #include <stdlib.h>
 
 #define ITEM_GOLD   0
-#define ITEM_FOOD   1
+#define ITEM_WEAPON 1
 #define ITEM_POTION 2
 #define ITEM_SCROLL 3
+
+#define DAMAGE_BLUNT 0
+#define DAMAGE_SLASH 1
+#define DAMAGE_SILVER 2
 
 #define ITEM_EFFECT_NONE 0
 #define ITEM_EFFECT_HEAL 1
 #define ITEM_EFFECT_TELEPORT 2
 #define ITEM_EFFECT_DAMAGE 3
 
+// for potions & scrolls
+typedef struct {
+    int effect; // one of ITEM_EFFECT consts
+    unsigned int strength; // strength of EFFECT
+} ItemAttributes;
+
+// for weapons
+typedef struct {
+    int min;
+    int max;
+    int type; // one of DAMAGE consts
+    int twoHanded; // boolean if this is two handed
+} WeaponAttributes;
+
 typedef struct {
     int type; // one of ITEM consts
-    int effect; // one of ITEM_EFFECT consts (for potions/scrolls)
-    unsigned int strength; // strength of EFFECT or amount of $
-    const char *unknownName; // random (unidentified) name
     const char *name; // real (identified) name
+    const char *unknownName; // random (unidentified) name
+    union {
+        int amount;
+        ItemAttributes attributes;
+        WeaponAttributes damage;
+    };
 } Item;
 
 typedef struct {
@@ -27,8 +48,11 @@ typedef struct {
     Item **content;
 } Items;
 
+const char **unknownItems;
+const char **knownItems;
+
 // return a random item for the specified dungeon depth
-Item *create_item(int depth, int type, const char **unknownItems, const char **knownItems);
+Item *create_item(int depth, int type);
 
 // initialize our item list
 Items initialize_items();
