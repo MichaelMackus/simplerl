354120ce23934d37cb8f489dc19e531447f2b71b
Michael Mackus <michaelmackus@gmail.com>
2018-10-09 08:49:09 -0700

    Simplify corridor generation

--- dungeon.c
+++ dungeon.c
@@ -115,7 +115,7 @@ int is_empty(Coords coords)
 
 Box *random_cell(Level *level);
 void fill_cell(Box *cell, Level *level, int fill);
-void randomly_fill_corridors(Level *level, const Box **cells, int startIndex, int cellCount);
+void randomly_fill_corridors(Level *level, const Box **cells, int cellCount);
 int branches(const Box *cell, const Level *level);
 void randomly_fill_tiles(Level *level)
 {
@@ -140,12 +140,10 @@ void randomly_fill_tiles(Level *level)
     }
 
     // fill corridors between cells
-    randomly_fill_corridors(level, (const Box**) cells, 0, cellCount);
+    randomly_fill_corridors(level, (const Box**) cells, cellCount);
 
     // randomly place upstairs
     i = generate(0, cellCount - 1);
-    while (branches(cells[i], level) == 0) // ensure we pick valid cell for upstairs
-        i = generate(0, cellCount - 1);
     Coords up;
     up.x = cells[i]->coords.x + cells[i]->dimensions.w/2;
     up.y = cells[i]->coords.y + cells[i]->dimensions.h/2;
@@ -155,20 +153,11 @@ void randomly_fill_tiles(Level *level)
     // TODO place downstairs at greater distance from upstairs
     // TODO once win condition is defined, don't place downstairs on last level
     int j = i;
+    while (i == j)
+        i = generate(0, cellCount - 1);
     Coords down;
-    while (i == j || branches(cells[j], level) == 0) // ensure we pick valid cell for downstairs
-    {
-        j = generate(0, cellCount - 1);
-        down.x = cells[j]->coords.x + cells[j]->dimensions.w/2;
-        down.y = cells[j]->coords.y + cells[j]->dimensions.h/2;
-
-        // try again if up & down stairs don't connect, TODO endless loop?
-        const Coords **path = find_path(up, down, (const Tile**) level->tiles);
-        if (path == NULL)
-            j = i;
-        else
-            free_path(path);
-    }
+    down.x = cells[i]->coords.x + cells[i]->dimensions.w/2;
+    down.y = cells[i]->coords.y + cells[i]->dimensions.h/2;
     level->tiles[down.y][down.x].type = TILE_STAIR_DOWN;
 
     // free cells
@@ -204,96 +193,35 @@ void randomly_fill_mobs(Level *level, int max)
 
 int is_path_clear(const Coords start, const Coords target, const Box **cells, int cellCount);
 void draw_line(const Coords a, const Coords b, Level *level);
-void randomly_fill_corridors(Level *level, const Box **cells, int startIndex, int cellCount)
+int branches(const Box *cell, const Level *level);
+Coords find_random_section(const Box *cell);
+void randomly_fill_corridors(Level *level, const Box **cells, int cellCount)
 {
-    /**
-     *
-     * RANDOM CORRIDOR GENERATION PSEUDO-CODE
-     *
-     * neighbor:
-     *  cell where the a* line from centers does not pass through
-     *  another cell
-     * branch:
-     *  space in wall of cell, connecting to another cell
-     *
-     * LOOP:
-     *  Pick cell with no branches
-     *  Pick neighbor (with at most 1 branch) at random
-     *  Draw corridor to neighbor
-     *  Start loop over with neighbor as cell
-     *
-     * DONE:
-     *  Remove non-connected cells
-     *
-     */
-
-    // can't fill corridor if no start cell
-    if (startIndex >= cellCount || cells[startIndex] == NULL)
-        return;
-
-    // check for possible neighbor, and draw line to its closest wall
-    for (int i = 0; i < cellCount; ++i) // check cells other than start cell
-    {
-        // neighbor can have at most 1 branch
-        if (branches(cells[i], level) > 1)
-            continue;
-
-        // check we're not comparing same cell
-        if (i == startIndex)
-            continue;
-
-        // try *all* walls to target
-        Coords start;
-        for (int j = 1; j <= 4; ++j)
-        {
-            switch (j)
-            {
-                case 1:
-                    // left wall
-                    start.x = cells[startIndex]->coords.x;
-                    start.y = cells[startIndex]->coords.y + (cells[startIndex]->dimensions.h / 2);
-                    break;
-                case 2:
-                    // top wall
-                    start.x = cells[startIndex]->coords.x + (cells[startIndex]->dimensions.w / 2);
-                    start.y = cells[startIndex]->coords.y;
-                    break;
-                case 3:
-                    // right wall
-                    start.x = cells[startIndex]->coords.x + cells[startIndex]->dimensions.w - 1;
-                    start.y = cells[startIndex]->coords.y + (cells[startIndex]->dimensions.h / 2);
-                    break;
-                case 4:
-                    // bottom wall
-                    start.x = cells[startIndex]->coords.x + (cells[startIndex]->dimensions.w / 2);
-                    start.y = cells[startIndex]->coords.y + cells[startIndex]->dimensions.h - 1;
-                    break;
-            }
-
-            Coords target;
-            target.x = cells[i]->coords.x + (cells[i]->dimensions.w / 2);
-            target.y = cells[i]->coords.y + (cells[i]->dimensions.h / 2);
-
-            // pick neighbor's closest wall
-            if (j == 1) // left
-                target.x = cells[i]->coords.x + cells[i]->dimensions.w - 1; // right
-            else if (j == 2) // top
-                target.y = cells[i]->coords.y + cells[i]->dimensions.h - 1; // bottom
-            else if (j == 3) // right
-                target.x = cells[i]->coords.x; // left
-            else if (j == 4) // bottom
-                target.y = cells[i]->coords.y; // top
-
-            // check for clear line from start to target
-            if (is_path_clear(start, target, cells, cellCount))
-            {
-                draw_line(start, target, level);
-                break;
-            }
+    // ensure at least 1 cell has no branches
+    int unreachableCell = 0;
+    for (int i = 0; i < cellCount; ++i) {
+        if (branches(cells[i], level) == 0) {
+            unreachableCell = 1;
+            break;
         }
     }
 
-    return randomly_fill_corridors(level, cells, startIndex + 1, cellCount);
+    if (unreachableCell == 0) {
+        // done
+        return;
+    }
+
+    int a = generate(0, cellCount - 1);
+    int b = generate(0, cellCount - 1);
+
+    if (a != b) {
+        Coords start = find_random_section(cells[a]);
+        Coords target = find_random_section(cells[b]);
+
+        draw_line(start, target, level);
+    }
+
+    randomly_fill_corridors(level, cells, cellCount);
 }
 
 /*************/
@@ -304,13 +232,15 @@ void randomly_fill_corridors(Level *level, const Box **cells, int startIndex, in
 
 // draws a line using straight lines (just a simple right angle for now)
 // TODO should probably avoid carving into walls
+// TODO should probably avoid close corridors
 void draw_line(const Coords a, const Coords b, Level *level)
 {
     // x & y direction
     int dy = 0, dx = 0;
 
     // pick direction based on greatest difference between coords
-    if (abs(a.y - b.y) > abs(a.x - b.x))
+    int diffy = abs(a.y - b.y), diffx = abs(a.x - b.x);
+    if (diffy > diffx)
     {
         if (a.y > b.y)
             dy = -1; // go up
@@ -329,7 +259,7 @@ void draw_line(const Coords a, const Coords b, Level *level)
     Coords current;
     current.x = a.x;
     current.y = a.y;
-    do
+    while (1 == 1)
     {
         if (get_tile(level, current) == NULL)
             return;
@@ -337,69 +267,63 @@ void draw_line(const Coords a, const Coords b, Level *level)
         if (level->tiles[current.y][current.x].type != TILE_FLOOR)
             level->tiles[current.y][current.x].type = TILE_CAVERN;
 
-        /* if (current.y == b.y && dy != 0) */
-        /* { */
-        /*     dy = 0; */
-        /*     if (current.x > b.x) */
-        /*         dx = -1; // go left */
-        /*     else if (current.x < b.x) */
-        /*         dx = 1; // go right */
-        /* } */
-        /* else if (current.x == b.x && dx != 0) */
-        /* { */
-        /*     dx = 0; */
-        /*     if (current.y > b.y) */
-        /*         dy = -1; // go up */
-        /*     else if (current.y < b.y) */
-        /*         dy = 1; // go down */
-        /* } */
+        if (current.x == b.x && current.y == b.y)
+            return;
 
-        // switch direction based on current spot
-        if (dy < 0 && current.y + dy < b.y)
+        if (current.y == b.y && dy != 0)
         {
             dy = 0;
-            if (current.x < b.x)
-                dx = 1;
-            else if (current.x > b.x)
-                dx = -1;
-            else
-                dy = 1;
+            if (current.x > b.x)
+                dx = -1; // go left
+            else if (current.x < b.x)
+                dx = 1; // go right
         }
-        else if (dy > 0 && current.y + dy > b.y)
-        {
-            dy = 0;
-            if (current.x < b.x)
-                dx = 1;
-            else if (current.x > b.x)
-                dx = -1;
-            else
-                dy = -1;
-        }
-        else if (dx < 0 && current.x + dx < b.x)
+        else if (current.x == b.x && dx != 0)
         {
             dx = 0;
-            if (current.y < b.y)
-                dy = 1;
-            else if (current.y > b.y)
-                dy = -1;
-            else
-                dx = 1;
-        }
-        else if (dx > 0 && current.x + dx > b.x)
-        {
-            dx = 0;
-            if (current.y < b.y)
-                dy = 1;
-            else if (current.y > b.y)
-                dy = -1;
-            else
-                dx = -1;
+            if (current.y > b.y)
+                dy = -1; // go up
+            else if (current.y < b.y)
+                dy = 1; // go down
         }
 
         current.y += dy;
         current.x += dx;
     }
-    while (current.x != b.x || current.y != b.y);
+}
+
+Coords find_random_section(const Box *cell)
+{
+    Coords result;
+
+    // pick random wall
+    int j = generate(1, 4);
+
+    switch (j)
+    {
+        case 1:
+            // left wall
+            result.x = cell->coords.x;
+            result.y = cell->coords.y + (cell->dimensions.h / 2);
+            break;
+        case 2:
+            // top wall
+            result.x = cell->coords.x + (cell->dimensions.w / 2);
+            result.y = cell->coords.y;
+            break;
+        case 3:
+            // right wall
+            result.x = cell->coords.x + cell->dimensions.w - 1;
+            result.y = cell->coords.y + (cell->dimensions.h / 2);
+            break;
+        case 4:
+            // bottom wall
+            result.x = cell->coords.x + (cell->dimensions.w / 2);
+            result.y = cell->coords.y + cell->dimensions.h - 1;
+            break;
+    }
+
+    return result;
 }
 
 Coords random_coords(Level *level)
@@ -548,55 +472,6 @@ Dimensions random_dimensions()
     dimensions.h = possibleCellDimensions[i][1];
 
     return dimensions;
-}
-
-// checks for a clear straight line from center of start to center of target
-// return 1 if possible neighbor
-int within_cell(const Coords c, const Box cell);
-const Coords **get_line(const Coords a, const Coords b);
-void free_path(const Coords **line);
-int is_path_clear(const Coords start, const Coords target, const Box **cells, int cellCount)
-{
-    // draw straight line from start -> target
-    const Coords **line = get_line(start, target);
-
-    // check for any coord that passes through another cell
-    int i = 0;
-    while (line[i] != NULL)
-    {
-        // ensure we're not comparing start or target
-        if (!(line[i]->x == start.x && line[i]->y == start.y) && !(line[i]->x == target.x && line[i]->y == target.y))
-        {
-            for (int j = 0; j < cellCount; ++j)
-            {
-                const Box *cell = cells[j];
-
-                // if we're within *another* cell, this is no neighbor
-                if (within_cell(*line[i], *cell))
-                {
-                    free_path(line);
-
-                    return 0;
-                }
-            }
-        }
-        ++i;
-    }
-
-    free_path(line);
-
-    return 1;
-}
-
-// return 1 if coords are within cell
-int within_cell(const Coords c, const Box cell)
-{
-    for (int y = cell.coords.y; y < cell.coords.y + cell.dimensions.h; ++y)
-        for (int x = cell.coords.x; x < cell.coords.x + cell.dimensions.w; ++x)
-            if (c.x == x && c.y == y)
-                return 1;
-
-    return 0;
 }
 
 // calculate amount of branches (i.e. holes in cell walls)
