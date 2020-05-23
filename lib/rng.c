acb76b5852cb869b7e9aeb61adc763f6690027f1
Michael Mackus <michaelmackus@gmail.com>
2020-05-23 13:22:51 -0700

    Change RNG type to unsigned long

--- lib/rng.c
+++ lib/rng.c
@@ -9,7 +9,7 @@ void rl_rng_stdlib_init()
     srand(time(0));
 }
 
-int rl_rng_stdlib_generate(int min, int max)
+unsigned long rl_rng_stdlib_generate(unsigned long min, unsigned long max)
 {
     // TODO assert/unsigned?
     if (min < 0 || max < 0 || (min == 0 && max == 0))
@@ -32,12 +32,12 @@ void rl_rng_twister_free()
     if (rl_twister_state) mtwist_free(rl_twister_state);
 }
 
-int rl_rng_twister_generate(int min, int max)
+unsigned long rl_rng_twister_generate(unsigned long min, unsigned long max)
 {
     // TODO assert/unsigned?
     if (min < 0 || max < 0 || (min == 0 && max == 0))
         return 0;
 
-    return min + (int)(mtwist_drand(rl_twister_state) * (max - min + 1));
+    return min + mtwist_drand(rl_twister_state) * (max - min + 1);
 }
 #endif
--- lib/rng.h
+++ lib/rng.h
@@ -1,19 +1,19 @@
 #ifndef RL_RNG_H
 #define RL_RNG_H
 
-typedef int (*rl_generator_f)(int min, int max);
+typedef unsigned long (*rl_generator_f)(unsigned long min, unsigned long max);
 
 // simple RNG implementation using C standard library
 // NOTE: you probably want to use something else for best results on all systems
 void rl_rng_stdlib_init();
-int rl_rng_stdlib_generate(int min, int max);
+unsigned long rl_rng_stdlib_generate(unsigned long min, unsigned long max);
 
 #ifdef RL_TWISTER
 // RNG implementation using mersenne twister
 // uses https://github.com/dajobe/libmtwist
 void rl_rng_twister_init();
 void rl_rng_twister_free();
-int rl_rng_twister_generate(int min, int max);
+unsigned long rl_rng_twister_generate(unsigned long min, unsigned long max);
 #endif
 
 #endif
