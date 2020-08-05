4eb800b91108282b09de69af36d74464a3ab7323
Michael Mackus <michaelmackus@gmail.com>
2020-08-04 19:38:34 -0700

    Add deviation argument & document function
    This allows control over deviation from center line.

--- lib/mapgen.c
+++ lib/mapgen.c
@@ -1,16 +1,19 @@
 #include "mapgen.h"
 
+#include <assert.h>
+
 #define MAX_GENERATOR_RECURSION 10
 
 void rl_recursively_split_bsp(rl_bsp *root, rl_generator_f generator,
-        unsigned int min_width, unsigned int min_height, int max_recursion)
+        unsigned int min_width, unsigned int min_height, float deviation, int max_recursion)
 {
     if (max_recursion <= 0)
         return;
 
+    assert(deviation >= 0.0 && deviation <= 1.0);
+
     unsigned int width = rl_get_bsp_width(root);
     unsigned int height = rl_get_bsp_height(root);
-
     unsigned int max_width = width - min_width;
     unsigned int max_height = height - min_height;
 
@@ -28,21 +31,38 @@ void rl_recursively_split_bsp(rl_bsp *root, rl_generator_f generator,
             dir = RL_SPLIT_VERTICALLY;
     }
 
+    unsigned int split_position;
     if (dir == RL_SPLIT_HORIZONTALLY) {
         // cannot split if current node size is too small
         if (width < min_width*2)
             return;
 
-        int split_width = generator(min_width, max_width);
-        rl_split_bsp(root, split_width, RL_SPLIT_HORIZONTALLY);
+        unsigned int center = width / 2;
+        unsigned int from = center - (center * deviation);
+        unsigned int to = center + (center * deviation);
+        if (from < min_width)
+            from = min_width;
+        if (to > max_width)
+            to = max_width;
+
+        split_position = generator(from, to);
     } else {
         // cannot split if current node size is too small
         if (height < min_height*2)
             return;
 
-        int split_height = generator(min_height, max_height);
-        rl_split_bsp(root, split_height, RL_SPLIT_VERTICALLY);
+        unsigned int center = height / 2;
+        unsigned int from = center - (center * deviation);
+        unsigned int to = center + (center * deviation);
+        if (from < min_height)
+            from = min_height;
+        if (to > max_height)
+            to = max_height;
+
+        split_position = generator(from, to);
     }
+
+    rl_split_bsp(root, split_position, dir);
 
     // continue recursion
     rl_bsp *left = rl_get_bsp_left(root);
@@ -52,7 +72,7 @@ void rl_recursively_split_bsp(rl_bsp *root, rl_generator_f generator,
         return;
 
     rl_recursively_split_bsp(left, generator,
-            min_width, min_height, max_recursion - 1);
+            min_width, min_height, deviation, max_recursion - 1);
     rl_recursively_split_bsp(right, generator,
-            min_width, min_height, max_recursion - 1);
+            min_width, min_height, deviation, max_recursion - 1);
 }
--- lib/mapgen.h
+++ lib/mapgen.h
@@ -5,7 +5,18 @@
 #include "rng.h"
 #include "map.h"
 
+/**
+ * root is the root node to split from
+ * generator is your RNG generator function
+ * min_width defines the minimum width to split
+ * min_height defines the minimum height to split
+ * deviation is a a float value from 0.0-1.0 of how much the split can
+ *     deviate from center (1 means the split can appear anywhere within
+ *     the cell, and 0 means the split will always occur in the center)
+ * max_recursion limits the split recursion
+ */
 void rl_recursively_split_bsp(rl_bsp *root, rl_generator_f generator,
-        unsigned int min_width, unsigned int min_height, int max_recursion);
+        unsigned int min_width, unsigned int min_height,
+        float deviation, int max_recursion);
 
 #endif
