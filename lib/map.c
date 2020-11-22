3ea5d58ee140d153c67023b79b29bbe620c69663
Michael Mackus <michaelmackus@gmail.com>
2020-11-22 11:37:27 -0800

    Use separate tile flags array
    Passable flag introduced a bug on x86_64

--- lib/map.c
+++ lib/map.c
@@ -5,8 +5,11 @@
 struct rl_map {
     size_t width;
     size_t height;
-    char *tiles; // 2d array of tiles
+    rl_tile *tiles; // 2d array of tiles
+    char *tile_flags; // 2d array of tile flags
 };
+
+#define RL_TILE_FLAG_PASSABLE 1
 
 rl_map *rl_create_map(size_t width, size_t height)
 {
@@ -20,9 +23,10 @@ rl_map *rl_create_map(size_t width, size_t height)
 
     map->width = width;
     map->height = height;
-    map->tiles = calloc(width * height, sizeof(char));
+    map->tiles = calloc(width * height, sizeof(rl_tile));
+    map->tile_flags = calloc(width * height, sizeof(char));
 
-    if (map->tiles == NULL) {
+    if (map->tiles == NULL || map->tile_flags == NULL) {
         free(map);
 
         return NULL;
@@ -39,6 +43,9 @@ void rl_free_map(rl_map *map)
     if (map->tiles)
         free(map->tiles);
 
+    if (map->tile_flags)
+        free(map->tile_flags);
+
     free(map);
 }
 
@@ -54,7 +61,7 @@ int rl_is_passable(const rl_map *map, rl_coords loc)
     if (index >= map->width * map->height)
         return 0;
 
-    return map->tiles && map->tiles[index] & 0x80;
+    return map->tile_flags && map->tile_flags[index] & RL_TILE_FLAG_PASSABLE;
 }
 
 size_t rl_get_map_width(const rl_map *map)
@@ -83,7 +90,7 @@ void rl_set_passable(rl_map *map, rl_coords loc)
 
     size_t index = loc.y * map->width + loc.x;
     if (index < map->width * map->height) {
-        map->tiles[index] |= 0x80;
+        map->tile_flags[index] |= RL_TILE_FLAG_PASSABLE;
     }
 }
 
@@ -96,8 +103,8 @@ void rl_set_impassable(rl_map *map, rl_coords loc)
         return;
 
     size_t index = loc.y * map->width + loc.x;
-    if (index < map->width * map->height) {
-        map->tiles[index] ^= 0x80;
+    if (index < map->width * map->height && map->tile_flags[index] & RL_TILE_FLAG_PASSABLE) {
+        map->tile_flags[index] ^= RL_TILE_FLAG_PASSABLE;
     }
 }
 
@@ -115,9 +122,6 @@ rl_tile rl_get_tile(const rl_map *map, rl_coords loc)
 
     if (!map->tiles)
         return RL_TILE_INVALID;
-
-    if (map->tiles[index] & 0x80)
-        return map->tiles[index] ^ 0x80;
 
     return map->tiles[index];
 }
