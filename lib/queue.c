eaaa6636ee7133f82dbca0d98102cedc340ae055
Michael Mackus <michaelmackus@gmail.com>
2021-08-16 22:09:54 -0700

    Add function to transform to array

--- lib/queue.c
+++ lib/queue.c
@@ -1,7 +1,10 @@
 #include "queue.h"
 
 #include <stdlib.h>
+#include <assert.h>
 
+// TODO any way to have better error checking when user passes only a
+// TODO pointer? (and not pointer to pointer?)
 void *rl_pop(rl_queue **queue)
 {
     if (*queue == NULL)
@@ -61,4 +64,54 @@ void *rl_peek(const rl_queue *queue)
         return NULL;
 
     return queue->data;
+}
+
+int rl_queue_size(const rl_queue *queue)
+{
+    // get size of queue
+    int length = 0;
+    rl_queue *tip = queue;
+    while (tip) {
+        ++length;
+        tip = tip->next;
+    }
+
+    return length;
+}
+
+void **rl_queue_to_array(rl_queue **queue)
+{
+    // get size of queue
+    int data_size = 0;
+    rl_queue *tip = *queue;
+    while (tip) {
+        data_size += sizeof(tip->data);
+        tip = tip->next;
+    }
+
+    if (data_size <= 0) {
+        // free queue
+        while (rl_pop(queue)) {}
+
+        return NULL;
+    }
+
+    void **arr = malloc(data_size);
+
+    if (arr == NULL) {
+        // free queue
+        while (rl_pop(queue)) {}
+
+        return NULL;
+    }
+
+    void **current = arr;
+    void *data;
+    int i = 0;
+    while (data = rl_pop(queue)) {
+        *current = data;
+        ++current;
+    }
+
+    return arr;
 }
--- lib/queue.h
+++ lib/queue.h
@@ -9,6 +9,7 @@ struct rl_queue {
 typedef struct rl_queue rl_queue;
 
 // push to queue - if (*queue) == NULL this allocates & initializes the queue
+// to free the queue use the "rl_pop" function until it returns NULL
 void rl_push(rl_queue **queue, void *data, int priority);
 
 // pop element from queue and free the old queue node
@@ -16,5 +17,11 @@ void *rl_pop(rl_queue **queue);
 
 // peek at the data in the queue
 void *rl_peek(const rl_queue *queue);
+
+// return length of queue
+int rl_queue_size(const rl_queue *queue);
+
+// transform queue to array - make sure you free the array when you are done
+void **rl_queue_to_array(rl_queue **queue);
 
 #endif
